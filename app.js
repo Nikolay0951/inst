83bc6ff7f24b78b0882c11a0bcff17d1f9b24bfc
Blalala <student>
2019-09-28 11:03:17 +0300

    переименование 2

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 console.log('worked');
 
-const buzova = {
+const buzova2 = {
     id: 'bozova86',
     avatarUrl: 'https://scontent-arn2-1.cdninstagram.com/vp/e24c6f401983834e27247c3542022660/5E1B7CB0/t51.2885-19/s150x150/69672885_368328154091538_8485268536803983360_n.jpg?_nc_ht=scontent-arn2-1.cdninstagram.com',
     verified: true,
@@ -8,7 +8,7 @@ const buzova = {
 };
 const photo = {
     id: 'B23d82NooNF',
-    author: buzova,
+    author: buzova2,
     location: 'Набережная Амура',
     imageUrl: 'https://placekitten.com/800/800',
     videoUrl: null,
@@ -17,7 +17,7 @@ const photo = {
 };
 const video = {
     id: 'B24FlbJos5E',
-    author: buzova,
+    author: buzova2,
     location: 'ТРЦ центр острова',
     imageUrl: 'https://placekitten.com/800/800',
     videoUrl: 'https://youtube.com/...',
